689194caffb7f3b35d9be8c7875acb6cdf5e61f0
Maher <maher91syr@gmail.com>
2024-10-31 00:11:12 +0900

    deploy layout fixes

--- src/components/TopPage.js
+++ src/components/TopPage.js
@@ -24,6 +24,12 @@ const TopPage = () => {
       id: "ai-slider",
     },
     {
+      img: "/images/MemberSearch.png",
+      title: "メンバー検索",
+      description: "事業のコラボレーションにピッタリな人を見つける",
+      subtitle: "COMYの取扱説明書",
+    },
+    {
       img: "/images/BulletinBoard.png",
       title: "掲示板",
       description: "メンバーと気軽に繋がれるオンラインの居場所",
@@ -121,7 +127,7 @@ const TopPage = () => {
 
       <div className="features">
         <h2>COMYの今後の追加機能紹介</h2>
-        {slides.map((slide) => (
+        {slides.slice(0, 3).map((slide) => (
           <div
             className="photo photo-common disable-select"
             key={slide.id || slide.title}
@@ -136,7 +142,7 @@ const TopPage = () => {
         ))}
       </div>
 
-      <div className="links">
+      {/* <div className="links">
         <h2>ニュース</h2>
         <ul>
           {blogs?.map((item) => (
@@ -145,7 +151,7 @@ const TopPage = () => {
             </li>
           ))}
         </ul>
-      </div>
+      </div> */}
     </>
   );
 };
